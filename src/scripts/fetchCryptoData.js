7a6f9e1837bc8791a19b27b57597096eb2274a9f
gurur <kisaoglugurur@gmail.com>
2025-05-10 03:35:18 +0300

    Enhance section prices layout; double gap size, improve last update time styling, and adjust alignment

--- src/scripts/fetchCryptoData.js
+++ src/scripts/fetchCryptoData.js
@@ -33,7 +33,7 @@ export async function fetchCryptoData() {
           <div class="cryptocurrency-card-head">
             <div class="cryptocurrency-icon"><img src="${coin.image}" alt="${coin.name} icon"></div>
             <div class="cryptocurrency-name">${formattedCoinName}</div>
-            <div class="cryptocurrency-abbreviation">${coin.symbol.toUpperCase()}</div>
+            <div class="cryptocurrency-abbreviation">(${coin.symbol.toUpperCase()})</div>
           </div>
           <div class="cryptocurrency-card-body">
             <div class="cryptocurrency-price">${formattedCoinPrice}</div>
@@ -53,7 +53,7 @@ export async function fetchCryptoData() {
         <svg class="last-update-time-icon js-last-update-time-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
         </svg>
-        Last updated at: ${timeString}
+        Last update: ${timeString}
       `;
 
   } catch (error) {
