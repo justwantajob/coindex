e61bf3fbb9262e92132e928e03f6a717e1c9ed97
gurur <kisaoglugurur@gmail.com>
2025-05-10 04:01:23 +0300

    Refactor section prices header class name; add news section with styles; update main layout to use grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
+// IMPORTING JS
+import { fetchCryptoData } from './fetchCryptoData.js';
+
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 /* CSS FILES FOR COMPONENTS GOES HERE */
 import '../styles/components/header.css';
 import '../styles/components/section-prices.css';
+import '../styles/components/section-news.css';
 import '../styles/utils.css';
 
-import { fetchCryptoData } from './fetchCryptoData.js';
 fetchCryptoData();
